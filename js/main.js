ff1651875c9ec9ad2981e7bf02693eca5410aa6f
chanchal78 <chanchallm78@gmail.com>
2026-03-09 15:52:38 +0600

    made the navbar, tab section, and the issue count bar, styled them

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
-// console.log("hello world");
+console.log("hello main");
 
-const signInBtn = document.getElementById('sign-in-btn');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-
-signInBtn.addEventListener('click', function(){
-    if(username.value === 'admin' && password.value === 'admin123'){
-        window.location.href = 'main.html';
-    }
-    else{
-        const errorMessage = document.getElementById('error-message');
-        errorMessage.classList.remove('hidden');
-    }
-});
+// document.querySelectorAll('#tabs-section button').forEach(button => {
+//     button.classList.add('btn-primary');
+// });
