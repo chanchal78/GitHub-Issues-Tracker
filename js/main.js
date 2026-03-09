ff8a36fcc141c6fb0df3482c8784cdd40c9b307c
chanchal78 <chanchallm78@gmail.com>
2026-03-09 21:03:56 +0600

    modified the big functions to be smaller easy to read functions, styled teh cards

--- js/main.js
+++ js/main.js
@@ -16,54 +16,124 @@ async function loadCards(){
     const data = await res.json();
     console.log(data);
     // console.log(cardContainer);
+
+    allIssues = data.data;
     displayCards(allIssues);
 }
 
 loadCards();
 
-//for displaying cards
+//display card
 function displayCards(cards){
-    cardContainer.innerHTML = "";
 
+    cardContainer.innerHTML = "";
     issueCount.innerText = cards.length + " Issues";
 
     cards.forEach(issue => {
-        const createCard = document.createElement('div');
+        const card = createCard(issue);
+        cardContainer.appendChild(card);
+    });
+}
 
-        const borderColor = issue.status === 'open' ? 'border-t-4 border-green-500' : 'border-t-4 border-purple-500';
-        
-        createCard.innerHTML = `
-        <div class="bg-white shadow-sm ${borderColor} rounded-md">
+function createCard(issue){
 
-            <div class="p-5">
+    const card = document.createElement("div");
 
-                <div class="flex justify-between items-center">
-                    <p class="font-semibold text-sm">${issue.status.toUpperCase()}</p>
-                    <div class="badge badge-outline">${issue.priority}</div>
-                </div>
+    const borderColor = getBorderColor(issue.status);
+    const icon = getStatusIcon(issue.status);
+    const priority = getPriorityBadge(issue.priority);
+    const labels = generateLabels(issue.labels);
 
-                <h2 class="text-lg font-semibold mt-2">${issue.title}</h2>
+    card.innerHTML = `
+    <div class="bg-white shadow-sm ${borderColor} rounded-lg h-full flex flex-col">
 
-                <p class="text-gray-500 text-sm line-clamp-2 mt-1">
-                    ${issue.description}
-                </p>
+        <div class="p-5 flex-grow">
 
-                <div class="flex gap-2 mt-3">
-                    <div class="badge badge-soft badge-secondary">${issue.label}</div>
-                </div>
-
+            <div class="flex justify-between items-center">
+                <img class="w-6 h-6" src="${icon}">
+                ${priority}
             </div>
 
-            <div class="border-t p-4 text-sm text-gray-500 flex justify-between">
-                <p>#${issue.id} by ${issue.author}</p>
-                <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+            <h2 class="text-[14px] font-semibold mt-3 line-clamp-2">
+                ${issue.title}
+            </h2>
+
+            <p class="text-gray-500 text-[12px] mt-2 line-clamp-2">
+                ${issue.description}
+            </p>
+
+            <div class="flex text-[12px] gap-2 mt-3">
+                ${labels}
             </div>
 
         </div>
-        `;
 
-        cardContainer.appendChild(createCard);
+        <div class="border-t px-5 py-4 text-[12px] text-gray-500 mt-auto">
+            <p>#${issue.id} by ${issue.author}</p>
+            <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+        </div>
+
+    </div>
+    `;
+
+    card.classList.add("cursor-pointer");
+
+    card.addEventListener("click", () => {
+        alert(issue.title + "\n\n" + issue.description);
     });
+    return card;
+}
+
+function getBorderColor(status){
+
+    if(status === "open"){
+        return "border-t-4 border-green-500";
+    }
+    return "border-t-4 border-purple-500";
+}
+
+function getStatusIcon(status){
+
+    if(status === "open"){
+        return "assets/Open-Status.png";
+    }
+    return "assets/Closed- Status .png";
+}
+
+function getPriorityBadge(priority){
+
+    if(priority === "high"){
+        return `<div class="badge badge-soft badge-secondary">High</div>`;
+    }
+    if(priority === "medium"){
+        return `<div class="badge badge-soft badge-warning">Medium</div>`;
+    }
+    return `<div class="badge badge-soft badge-primary">Low</div>`;
+}
+
+//labels 
+function generateLabels(labels){
+
+    return labels.map(label => {
+
+        if(label === "bug"){
+            return `<div class="badge badge-outline bg-red-100 badge-error text-[12px]">BUG</div>`;
+        }
+        if(label === "help wanted"){
+            return `<div class="badge badge-outline bg-yellow-100 badge-warning text-[12px]">HELP WANTED</div>`;
+        }
+        if(label === "enhancement"){
+            return `<div class="badge badge-outline bg-green-100 badge-success text-[12px]">ENHANCEMENT</div>`;
+        }
+        if(label === "good first issue"){
+            return `<div class="badge badge-outline bg-blue-100 badge-info text-[12px]">Good First Issue</div>`;
+        }
+        if(label === "documentation"){
+            return `<div class="badge badge-outline bg-teal-100 badge-accent text-[12px]">Documentation</div>`;
+        }
+        return `<div class="badge badge-outline">${label}</div>`;
+
+    }).join("");
 }
 
 
