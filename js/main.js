cadfe4a64bf78ce5f7cbbb93a70e1be003ca8bb7
chanchal78 <chanchallm78@gmail.com>
2026-03-09 18:20:07 +0600

    did the function for lodingCards and displaycard

--- js/main.js
+++ js/main.js
@@ -1,5 +1,68 @@
 console.log("hello main");
 
-// document.querySelectorAll('#tabs-section button').forEach(button => {
-//     button.classList.add('btn-primary');
-// });
+const cardContainer = document.getElementById('cards-container');
+const issueCount = document.querySelector("#issues-section h2");
+
+const allBtn = document.querySelector("#tabs-section button:nth-child(1)");
+const openBtn = document.querySelector("#tabs-section button:nth-child(2)");
+const closedBtn = document.querySelector("#tabs-section button:nth-child(3)");
+
+let allIssues = [];
+
+//to load all issues
+async function loadCards(){
+
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const data = await res.json();
+    console.log(data);
+    // console.log(cardContainer);
+    displayCards(allIssues);
+}
+
+loadCards();
+
+//for displaying cards
+async function displayCards(cards){
+    cardContainer.innerHTML = "";
+
+    issueCount.innerText = cards.length + " Issues";
+
+    cards.forEach(issue => {
+        const createCard = document.createElement('div');
+
+        const borderColor = issue.status === 'open' ? 'border-t-4 border-green-500' : 'border-t-4 border-purple-500';
+        
+        createCard.innerHTML = `
+        <div class="bg-white shadow-sm ${borderColor} rounded-md">
+
+            <div class="p-5">
+
+                <div class="flex justify-between items-center">
+                    <p class="font-semibold text-sm">${issue.status.toUpperCase()}</p>
+                    <div class="badge badge-outline">${issue.priority}</div>
+                </div>
+
+                <h2 class="text-lg font-semibold mt-2">${issue.title}</h2>
+
+                <p class="text-gray-500 text-sm line-clamp-2 mt-1">
+                    ${issue.description}
+                </p>
+
+                <div class="flex gap-2 mt-3">
+                    <div class="badge badge-soft badge-secondary">${issue.label}</div>
+                </div>
+
+            </div>
+
+            <div class="border-t p-4 text-sm text-gray-500 flex justify-between">
+                <p>#${issue.id} by ${issue.author}</p>
+                <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+            </div>
+
+        </div>
+        `;
+
+        cardContainer.appendChild(createCard);
+    });
+}
+
